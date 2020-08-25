11c5f3223e830eac0b94fcfc8718ae09fb053e53
Christopher <christopherstoy@gmail.com>
2020-08-25 19:42:25 +0000

    updated function

--- spinner1.js
+++ spinner1.js
@@ -15,17 +15,21 @@ setTimeout(() => {
 }, 700);
 
 setTimeout(() => {
-  process.stdout.write('\r/  ');
+  process.stdout.write('\r|  ');
 }, 900);
 
 setTimeout(() => {
-  process.stdout.write('\r-  ');
+  process.stdout.write('\r/  ');
 }, 1100);
 
 setTimeout(() => {
-  process.stdout.write('\r\\  ');
+  process.stdout.write('\r-  ');
 }, 1300);
 
 setTimeout(() => {
-  process.stdout.write('\r|  ');
+  process.stdout.write('\r\\  ');
 }, 1500);
+
+setTimeout(() => {
+  process.stdout.write('\r|  ');
+}, 1700);
